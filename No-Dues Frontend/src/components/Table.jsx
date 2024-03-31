d931088f77558373e34a7a5f30eb2ef353bb5a81
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2024-03-31 06:29:39 +0530

    Fixing Table

--- No-Dues Frontend/src/components/Table.jsx
+++ No-Dues Frontend/src/components/Table.jsx
@@ -14,27 +14,10 @@ import {
   IconButton,
 } from "@mui/material";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import GenericModal from "./GenericModal";
+import CreateDueForm from "./CreateDueForm";
 
-import { getDepartmentDue } from '../service/fetchDepartmentDue'
-
-export default function StickyHeadTable({rows}) {
-    const columns = [
-      {
-        id: "reason",
-        label: "Department",
-        minWidth: 100,
-      },
-      {
-        id: "amount",
-        label: "Amount",
-        minWidth: 100,
-      },
-      {
-        id: "due_date",
-        label: "Due Date",
-        minWidth: 100,
-      },
-    ];
+export default function StickyHeadTable({ rows, columns, isDep }) {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [anchorEl, setAnchorEl] = useState(null);
@@ -61,78 +44,109 @@ export default function StickyHeadTable({rows}) {
 
   return (
     <div className="container mx-auto">
-        <div className="w-full overflow-hidden mt-10">
-            <Paper sx={{ width: "100%", overflow: "hidden" }}>
-            <TableContainer sx={{ maxHeight: 440 }}>
-                <Table stickyHeader aria-label="sticky table">
-                <TableHead>
-                    <TableRow>
-                    {columns.map((column) => (
-                        <TableCell
-                        key={column.id}
-                        align={column.align}
-                        className="min-w-[100px]"
-                        >
-                        {column.label}
+      <div className="w-full overflow-hidden mt-10">
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>S. No.</TableCell>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      className="min-w-[100px]"
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                  <TableCell>Details</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row, rowIndex) => {
+                    return (
+                      <TableRow hover tabIndex={-1} key={rowIndex}>
+                        <TableCell>
+                          {rowIndex + 1}
                         </TableCell>
-                    ))}
-                    <TableCell>Details</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row, rowIndex) => {
-                        return (
-                        <TableRow hover tabIndex={-1} key={rowIndex}>
-                            {columns.map((column) => {
-                            const value = row[column.id];
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          if (column.id == 'status' && isDep == false) {
                             return (
-                                <TableCell key={column.id} align={column.align}>
+                              <TableCell key={column.id} align={column.align}>
+                                {row.status === "success" ? (
+                                  <Button variant="contained" style={{ backgroundColor: "green", color: "white" }}>
+                                    {row.status}
+                                  </Button>
+                                ) : row.status === "pending" ? (
+                                  <Button variant="contained" style={{ backgroundColor: "purple", color: "white" }}>
+                                    {row.status}
+                                  </Button>
+                                ) : row.status === "rejected" ? (
+                                  <Button variant="contained" style={{ backgroundColor: "red", color: "white" }}>
+                                    {row.status}
+                                  </Button>
+                                ) : (
+                                  row.status
+                                )}
+                              </TableCell>
+                            )
+                          } else {
+                            return (
+                              <TableCell key={column.id} align={column.align}>
                                 {column.format && typeof value === "number"
-                                    ? column.format(value)
-                                    : value}
-                                </TableCell>
+                                  ? column.format(value)
+                                  : value}
+                              </TableCell>
                             );
+                          }
+                        })}
+                        <TableCell>
+                          {isDep && <IconButton onClick={(event) => handleMenuOpen(event, row)}>
+                            <MoreVertIcon />
+                          </IconButton>}
+                          {isDep ? <Menu
+                            anchorEl={anchorEl}
+                            open={Boolean(anchorEl && selectedRow === row)}
+                            onClose={handleMenuClose}
+                          >
+                            {Object.keys(row).map((key) => {
+                              if (!columns.some((column) => column.id === key)) {
+                                return (
+                                  <MenuItem key={key} onClick={handleMenuClose}>
+                                    <strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong> {row[key]}
+                                  </MenuItem>
+                                );
+                              }
+                              return null;
                             })}
-                            <TableCell>
-                                <IconButton onClick={(event) => handleMenuOpen(event, row)}>
-                                    <MoreVertIcon/>
-                                </IconButton>
-                            <Menu
-                                anchorEl={anchorEl}
-                                open={Boolean(anchorEl && selectedRow === row)}
-                                onClose={handleMenuClose}
-                            >
-                                {Object.keys(row).map((key) => {
-                                if (!columns.some((column) => column.id === key)) {
-                                    return (
-                                    <MenuItem key={key} onClick={handleMenuClose}>
-                                        <strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong> {row[key]}
-                                    </MenuItem>
-                                    );
-                                }
-                                return null;
-                                })}
-                            </Menu>
-                            </TableCell>
-                        </TableRow>
-                        );
-                    })}
-                </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[2, 5, 10]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-            </Paper>
-        </div>
+                          </Menu> : <GenericModal
+                            buttonName="Open Modal"
+                            modalTitle="Example Modal"
+                          >
+                            <CreateDueForm />
+                          </GenericModal>}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[2, 5, 10]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </div>
     </div>
   );
 }
