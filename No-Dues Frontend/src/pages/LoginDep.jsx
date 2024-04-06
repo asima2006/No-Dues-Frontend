82761523187b32948ad29f06f1513a4768693789
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2024-04-06 16:32:08 +0530

    Completing Login UI and authentication

--- No-Dues Frontend/src/pages/LoginDep.jsx
+++ No-Dues Frontend/src/pages/LoginDep.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
-import { useSetRecoilState } from "recoil";
-import { authState, userTypeValues} from "../context/auth/authState";
+import React, { useEffect, useState } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { authState, userTypeValues } from "../context/auth/authState";
 import { useNavigate } from "react-router-dom";
 import { backendUri } from "../env";
+import { toast } from "react-toastify";
 
 const LoginDep = () => {
   const [formstate, setFormState] = useState({});
+  const context = useRecoilValue(authState);
   const setAuth = useSetRecoilState(authState);
+  const { token, userType } = context;
   const navigator = useNavigate()
 
   const handleChange = (e) => {
@@ -19,58 +22,78 @@ const LoginDep = () => {
   const handleDepPOST = async (e) => {
     e.preventDefault();
     try {
-
-      const response = await fetch(`${backendUri}/department/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: formstate.username,
-          password: formstate.password,
-        }),
-      });
-
-      if (response.ok) {
-        const { token } = await response.json();
-
-        setAuth({
-          isAuthenticated: true,
-          token: token,
-          userType: userTypeValues.department
-        });
-        console.log("Login successful");
-        navigator('/dues')
+      if (token != null || userType == userTypeValues.department) {
+        navigator("/dep");
       } else {
-        console.log("Login failed");
+        const response = await fetch(`${backendUri}/department/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: formstate.username,
+            password: formstate.password,
+          }),
+        });
+
+        if (response.ok) {
+          const { token } = await response.json();
+
+          setAuth({
+            isAuthenticated: true,
+            token: token,
+            userType: userTypeValues.department
+          });
+          toast('Login Successfully')
+          navigator('/dues')
+        } else {
+          toast('Login Failed')
+        }
+
       }
     } catch (error) {
       console.error("Error logging in:", error);
     }
   };
 
+  useEffect(()=>{
+
+  },[])
+
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">
-        Department Login
-      </h1>
-      <form className="flex flex-col gap-4" onSubmit={handleDepPOST}>
+    <div
+      id="departmentlogin"
+      className="text-right mt-6"
+    >
+      <form
+        onSubmit={handleDepPOST}
+        method="post"
+        className="flex flex-col "
+      >
         <input
           type="username"
           placeholder="Username"
-          className="border p-3 rounded-lg"
           id="username"
           onChange={handleChange}
+          className="h-10 border p-2 rounded-lg border-gray-400 mb-4 text-black outline-none"
         />
         <input
           type="password"
           placeholder="Password"
-          className="border p-3 rounded-lg"
           id="password"
           onChange={handleChange}
+          className="h-10 border p-2 rounded-lg border-gray-400 mt-4 mb-2 text-black outline-none"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Login
+
+        <a
+          href="#"
+          className="text-[#024182]  text-xs mb-2"
+        >
+          Forgot Password?
+        </a>
+
+        <button className="bg-blue-900 bg-opacity-97 rounded-lg px-4 py-2 align-center mt-4 mb-12 w-80 sm:w-[22rem] active:scale-[1.02]">
+          Sign In
         </button>
       </form>
     </div>
