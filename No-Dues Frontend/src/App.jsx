b7947b8883fb51ce3c945ad0389bd32ea16c89ef
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2024-04-07 11:53:12 +0530

    some changes

--- No-Dues Frontend/src/App.jsx
+++ No-Dues Frontend/src/App.jsx
@@ -10,14 +10,10 @@ import 'react-toastify/dist/ReactToastify.css';
 import { RecoilRoot } from 'recoil';
 import DepartmentLayout from './pages/_departmentLayout';
 import StudDues from './pages/StudDues';
-<<<<<<< HEAD
-import Certificate from './pages/Certificate';
-=======
 import CommunicationPage from './pages/Communication';
 import NotFoundPage from './pages/NotFound';
 import DepartmentCertificate from './pages/DepartmentCertificate';
 import DepartmentRequest from './pages/DepartmentRequests';
->>>>>>> aa765cae96cb381e663c7b386ed803cfad300aff
 
 const App = () => {
   return (
@@ -30,18 +26,14 @@ const App = () => {
             <Route path='/stud-dues' element={<StudDues />} />
             {/* <Route path='/login-dep' element={<LoginDep />} /> */}
             <Route path='/callback' element={<CallBack />} />
-<<<<<<< HEAD
             <Route path='/dep' element={<DepDash />} />
-            <Route path='/depComm' element={<Certificate />} />
           <Route path='/dues' element={<DepartmentLayout><Due /></DepartmentLayout>} />
-=======
             <Route path='/department-dashboard' element={<DepartmentLayout><DepDash /></DepartmentLayout>} />
             <Route path='/department-dues' element={<DepartmentLayout><Due /></DepartmentLayout>} />
             <Route path='/department-communication' element={<DepartmentLayout><CommunicationPage/></DepartmentLayout>} />
             <Route path='/department-certificates' element={<DepartmentLayout><DepartmentCertificate/></DepartmentLayout>} />
             <Route path='/department-requests' element={<DepartmentLayout><DepartmentRequest /></DepartmentLayout>} />
             <Route path='*' element={<NotFoundPage />} />
->>>>>>> aa765cae96cb381e663c7b386ed803cfad300aff
           </Routes>
         </BrowserRouter>
       <ToastContainer />
