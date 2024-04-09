3090cf7a680e9d40da1f254f99da37878410da99
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2024-04-10 00:39:57 +0530

    Modified routes

--- No-Dues Frontend/src/App.jsx
+++ No-Dues Frontend/src/App.jsx
@@ -8,7 +8,6 @@ import Due from './pages/Due';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { RecoilRoot } from 'recoil';
-import DepartmentLayout from './pages/_departmentLayout';
 import StudDues from './pages/StudDues';
 import CommunicationPage from './pages/Communication';
 import NotFoundPage from './pages/NotFound';
@@ -21,11 +20,9 @@ const App = () => {
       <RecoilRoot>
         <BrowserRouter>
           <Routes>
-            {/* <Route path='/' element={<Home />} /> */}
             <Route path='/' element={<New />} />
             <Route path='/stud' element={<StudDash />} />
             <Route path='/stud-dues' element={<StudDues />} />
-            {/* <Route path='/login-dep' element={<LoginDep />} /> */}
             <Route path='/callback' element={<CallBack />} />
             <Route path='/dep' element={<DepDash />} />
             <Route path='/dues' element={<Due />} />
